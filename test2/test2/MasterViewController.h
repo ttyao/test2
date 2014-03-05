3228968e8ce5c6f8f2a7478d3bb733a67fce0db8
Henry Yao <henryy@gmail.com>
2014-03-05 07:40:08 -0800

    test

--- test2/test2/MasterViewController.h
+++ test2/test2/MasterViewController.h
@@ -11,6 +11,7 @@
 @class DetailViewController;
 
 @interface MasterViewController : UITableViewController
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @property (strong, nonatomic) DetailViewController *detailViewController;
 
